ee6d55dd5d879470fbc2b7b0c90d9ed6f367dd00
schwarzam <gustavo.b.schwarz@gmail.com>
2022-10-04 16:08:12 -0300

    working on models and package

--- dados/package/models.py
+++ dados/package/models.py
@@ -1,8 +1,58 @@
 from sklearn.metrics import accuracy_score, classification_report
 from xgboost import XGBClassifier
+from sklearn.metrics import mean_squared_error
 
-def run_XGB(X_train, X_test, y_train, y_test, max_depth=2, gamma=0.3, eta=0.5, reg_alpha=0.5, reg_lambda=0.7):
-    print("Running XGB model.")
+import warnings
+warnings.filterwarnings("ignore")
+
+def run_analysis(predicted : list, y : list):
+    """Method to run simple analysis on predicted data
+
+    Args:
+        predicted (np.array, list): predictions
+        y (np.array, list): true values. 
+    """    
+    acc = accuracy_score(y, predicted)
+    print(classification_report(y, predicted))
+    print('Acc score: ', acc)
+    print('Mean Squared Error: ', mean_squared_error(y, predicted))
+    print('\n\n')
+
+def convertPredicted(pred : list, threshold : int = 0.5):
+    """Convert a list of predicted probabilities into 0 and 1 depending on the threshold.
+
+    You may also pass a list to create a double threshold. Numbers under the first element are 0. Bigger than the second element 1 and betwen both 2. 
+
+    Args:
+        pred (_type_): _description_
+        threshold (float, list): if flot if will be a simple threshold, if a list of two elements it creates two thresholds. Defaults to 0.5.
+
+    Returns:
+        list: converted predictions into integers
+    """    
+    ypred = []
+    if isinstance(threshold, float) or isinstance(threshold, int):
+        for i in pred:
+            if i > threshold:
+                ypred.append(1)
+            else:
+                ypred.append(0)
+                
+    elif isinstance(threshold, list):
+        if len(threshold) == 2:
+            for i in pred:
+                if i < threshold[0]:
+                    ypred.append(0)
+                elif i > threshold[1]:
+                    ypred.append(1)
+                else:
+                    ypred.append(2)
+    
+    return ypred
+
+def run_XGB(X_train, X_test, y_train, y_test, max_depth=2, gamma=0.3, eta=0.5, reg_alpha=0.5, reg_lambda=0.7, prints=False):
+    if prints:
+        print("Running XGB model.")
 
     model = XGBClassifier(
         max_depth=max_depth,
@@ -15,10 +65,11 @@ def run_XGB(X_train, X_test, y_train, y_test, max_depth=2, gamma=0.3, eta=0.5, r
     model.fit(X_train, y_train)
     
     pred = model.predict(X_test)
-    print(classification_report(pred, y_test))
-    
     acc = accuracy_score(pred, y_test)
-    print('accuracy_score: ', acc)
-    print('\n\n')
+
+    if prints:
+        print(classification_report(pred, y_test))
+        print('accuracy_score: ', acc)
+        print('\n\n')
     
-    return model
+    return model, acc 
---
+++ dados/package/process_dataset.py
@@ -0,0 +1,71 @@
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+import os
+
+def generate_labels(df, target = 'id_fechou'):
+    """Generate list with training columns.
+
+    Args:
+        df (pd.DataFrame): DataFrame, all columns except the target will be returned
+        target (str, optional): Traget column. Defaults to 'id_fechou'.
+
+    Returns:
+        tuple(list, str): list with training columns and target
+    """    
+    train_labels = list(df.columns)
+    train_labels.remove(target)
+    return train_labels, target
+
+
+def processColumns(df, remove_columns = ['Data_de_criacao', 'ano', 'ID_cliente', 'Codigo_da_oportunidade', 'Gestão da Segurança Pública', 
+                 'S_amp_OP_S_amp_OE', 'Transformação Digital', 'Roadmap'], create_columns=True):
+    
+    """Remove selected columns, and add columns if wanted.
+
+    Args:
+        df (pd.DataFrame): target dataframe
+        remove_columns (list, optional): Columns to remove. Defaults to ['Data_de_criacao', 'ano', 'ID_cliente', 'Codigo_da_oportunidade', 'Gestão da Segurança Pública', 'S_amp_OP_S_amp_OE', 'Transformação Digital', 'Roadmap'].
+        create_columns (bool, optional): add columns if wanted. Defaults to True.
+
+    Returns:
+        pd.DataFrame: modified DataFrame
+    """                 
+
+    if create_columns:
+        df["Custo_Total_per_Valor_corrigido2"] = df["Custo_Total"]/df["Valor_corrigido2"]
+        df["numero_relacionamentos_convertidos_per_numero_relacionamentos"] = df["numero_relacionamentos_convertidos"]/df["numero_relacionamentos"]
+        df["Gestão da Receita_per_Gestão de Gastos"] = df["Gestão da Receita"] + df["Gestão de Gastos"]
+
+    df = df.drop(remove_columns, axis=1)
+    return df
+
+
+def scaleData(df, save=True, useSaved=True):
+    """Scale data range using StandardScaler. This functions also saves the scaler so it may be used later.
+
+    Args:
+        df (pd.DataFrame): input dataframe to scale
+        save (bool, optional): save model. Defaults to True.
+        useSaved (bool, optional): load model. Defaults to True.
+
+    Returns:
+        np.array: scaled array.
+    """    
+
+    if useSaved:
+        print("Using saved scaler.")
+        try:
+            scaler = joblib.load(os.path.join('package/models', 'scaler.model'))
+        except: 
+            print("Scaler not found, creating a new one.")
+            useSaved = False
+
+    if not useSaved:
+        scaler = StandardScaler()
+        scaler.fit(df)
+
+    if save:
+        joblib.dump(scaler, os.path.join('package/models', 'scaler.model'))
+
+    return scaler.transform(df)
