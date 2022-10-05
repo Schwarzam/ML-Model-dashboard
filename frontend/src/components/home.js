8cd06be90dcad46a9b07e6e37671fda1194bd702
schwarzam <gustavo.b.schwarz@gmail.com>
2022-10-04 23:43:05 -0300

    working on frontend

---
+++ frontend/src/components/home.js
@@ -0,0 +1,179 @@
+import { ChevronRightIcon, StarIcon } from '@heroicons/react/20/solid'
+
+const stats = [
+  { label: 'Founded', value: '2021' },
+  { label: 'Employees', value: '5' },
+  { label: 'Beta Users', value: '521' },
+  { label: 'Raised', value: '$25M' },
+]
+const logos = [
+  { name: 'sklearn', url: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg' },
+  { name: 'django', url: 'https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg' },
+  { name: 'react', url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' },
+  { name: 'tensorflow', url: 'https://www.gstatic.com/devrel-devsite/prod/v24d520161c9661e427a3f6fa9973bfca56d0972dca82fa8ef65c709d915e80f3/tensorflow/images/lockup.svg' },
+]
+const footerNavigation = {
+  main: [
+    { name: 'About', href: '/about' },
+  ],
+  social: [
+
+    {
+      name: 'GitHub',
+      href: 'https://github.com/schwarzam/teros-hackaton',
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+
+  ],
+}
+
+export default function Home(){
+
+    return (
+        <div className="bg-white">
+      <main>
+        {/* Hero section */}
+        <div className="overflow-hidden pt-8 sm:pt-12 lg:relative lg:py-48">
+          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-24 lg:px-8">
+            <div>
+              <div>
+                <img
+                  className="h-11 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=rose&shade=500"
+                  alt="Your Company"
+                />
+              </div>
+              <div className="mt-20">
+                <div>
+                  <a href="#" className="inline-flex space-x-4">
+                    <span className="rounded bg-rose-50 px-2.5 py-1 text-sm font-semibold text-rose-500">
+                      proposal
+                    </span>
+                    <span className="inline-flex items-center space-x-1 text-sm font-medium text-rose-500">
+                      <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                    </span>
+                  </a>
+                </div>
+                <div className="mt-6 sm:max-w-xl">
+                  <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                    Hackaton
+                  </h1>
+                  <p className="mt-6 text-xl text-gray-500">
+                    
+The purpose of this work is to create an interface and a Machine Learning model that can classify certain data provided by the Teros company.
+                  </p>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+
+          <div className="sm:mx-auto sm:max-w-3xl sm:px-6">
+            <div className="py-12 sm:relative sm:mt-12 sm:py-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+              <div className="hidden sm:block">
+                <div className="absolute inset-y-0 left-1/2 w-screen rounded-l-3xl bg-gray-50 lg:left-80 lg:right-0 lg:w-full" />
+                <svg
+                  className="absolute top-8 right-1/2 -mr-3 lg:left-0 lg:m-0"
+                  width={404}
+                  height={392}
+                  fill="none"
+                  viewBox="0 0 404 392"
+                >
+                  <defs>
+                    <pattern
+                      id="837c3e70-6c3a-44e6-8854-cc48c737b659"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                    </pattern>
+                  </defs>
+                  <rect width={404} height={392} fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)" />
+                </svg>
+              </div>
+              <div className="relative -mr-40 pl-4 sm:mx-auto sm:max-w-3xl sm:px-0 lg:h-full lg:max-w-none lg:pl-12">
+                <img
+                  className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
+                  src="https://tailwindui.com/img/component-images/task-app-rose.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+
+        {/* Logo cloud section */}
+        <div className="mt-32">
+          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="lg:grid lg:grid-cols-2 lg:items-center lg:gap-24">
+              
+              <div className="mt-12 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
+                {logos.map((logo) => (
+                  <div key={logo.name} className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+                    <img className="max-h-12" src={logo.url} alt={logo.name} />
+                  </div>
+                ))}
+              </div>
+              <div className="pt-16 sm:pt-0">
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                    Created entirely with state-of-the-art technologies.
+                </h2>
+                <p className="mt-6 max-w-3xl text-lg leading-7 text-gray-500">
+                  Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque
+                  tristique pellentesque. Blandit amet, sed aenean erat arcu morbi. Cursus faucibus nunc nisl netus
+                  morbi vel porttitor vitae ut. Amet vitae fames senectus vitae.
+                </p>
+                <div className="mt-6">
+                  <a href="https://github.com/schwarzam/teros-hackaton" className="text-base font-medium text-rose-500">
+                    Checkout GitHub repository.
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      
+      </main>
+
+      {/* Footer section */}
+      <footer className="mt-24 bg-gray-900 sm:mt-12">
+        <div className="mx-auto max-w-md overflow-hidden py-12 px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+          <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
+            {footerNavigation.main.map((item) => (
+              <div key={item.name} className="px-5 py-2">
+                <a href={item.href} className="text-base text-gray-400 hover:text-gray-300">
+                  {item.name}
+                </a>
+              </div>
+            ))}
+          </nav>
+          <div className="mt-8 flex justify-center space-x-6">
+            {footerNavigation.social.map((item) => (
+              <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-300">
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))}
+          </div>
+          <p className="mt-8 text-center text-base text-gray-400">
+            &copy; 2020 Your Company, Inc. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
+    )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+import Home from './components/home';
+import About from './components/about';
+import Navbar from './components/navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/about" element={<About />} />
+          
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
